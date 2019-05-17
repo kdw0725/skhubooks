bedaf509321fb427efcc79803a1c261ff4c9e6f9
kdw0725 <aron0725@naver.com>
2019-05-17 18:03:21 +0900

    merge test

--- src/main/java/com/skhu/skhubooks/Controller/qnaController.java
+++ src/main/java/com/skhu/skhubooks/Controller/qnaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.skhu.skhubooks.Service.qnaService;
 import com.skhu.skhubooks.VO.qnaVO;
@@ -31,8 +32,32 @@ public class qnaController {
 	}
 	
 	@RequestMapping(value="/qna/qnaDelete", method  = RequestMethod.GET)
-	public String qnaDelete(Model model, qnaVO qnavo) {
-		
-		return "/qna/qnaDelete";
+	public String qnaDelete(Model model, qnaVO qnavo) throws Exception {
+		service.qnaDelete(qnavo.getQna_no());
+		return "redirect:/qna/qnaList";
+	}
+	
+	@RequestMapping(value="/qna/qnaUpdate", method = RequestMethod.POST)
+	@ResponseBody
+	public int qnaUpdate(qnaVO qnavo, String qna_content) throws Exception{
+		return service.qnaUpdate(qnavo);
+	}
+	
+	@RequestMapping(value="/qna/commentInsert", method = RequestMethod.POST)
+	@ResponseBody
+	public int commentInsert(qnaVO qnavo) throws Exception{
+		return service.commentInsert(qnavo);
+	}
+	
+	@RequestMapping(value="/qna/commentDelete", method = RequestMethod.GET)
+	public String commentDelete(int qna_no) throws Exception {
+		service.commentDelete(qna_no);
+		return "redirect:/qna/qnaList";
+	}
+	
+	@RequestMapping(value="/qna/commentUpdate", method = RequestMethod.POST)
+	@ResponseBody
+	public int commentUpdate(qnaVO qnavo) throws Exception{
+		return service.commentUpdate(qnavo);
 	}
 }
